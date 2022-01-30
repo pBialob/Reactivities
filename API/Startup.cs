a5839099be6a90131c7ce407ac1748917d2a081a
Piotr Białobrzeski <piotr.bialobrzeski01@gmail.com>
2022-01-30 15:08:03 +0100

    Created CRUD application and AutoMapper

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,7 +14,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using AutoMapper;
 using Persistence;
+using Application.Core;
+using API.Extenstions;
 
 namespace API
 {
@@ -29,21 +34,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyMethod().WithOrigins("http://localhost:3000");
-                });
-            });
+            ApplicationServiceExtenstions.AddApplicationServices(services, _config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
