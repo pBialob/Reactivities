6bd0aa37904ad3560c1992dedc582ed06b533f0b
Piotr Białobrzeski <piotr.bialobrzeski01@gmail.com>
2022-01-31 02:21:08 +0100

    CRUD application client-side with react

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css'
+import App from './app/layout/App';
 
 ReactDOM.render(
     <App />,
